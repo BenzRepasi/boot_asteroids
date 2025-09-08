215ee8731d0d1f86e9a3554ec12b0d1d607cbfb0
Bence <ben.rep@pop-os.localdomain>
2025-09-08 12:36:39 +0200

    Collision set

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
 import pygame
-import constants
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    delta_time = 0
+
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    #containers
+    Player.containers = (updatable, drawable) # type: ignore
+    Asteroid.containers = (asteroids, updatable, drawable) # type: ignore
+    AsteroidField.containers = updatable # type: ignore
+
+    #object inits
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.display.set_mode()
-    screen=pygame.display.set_mode([constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT])
+    screen=pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])    
+
+
 
     while(42):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(delta_time)
+
+        for ast in asteroids:
+            if ast.collision(player):
+                print("Game over!")
+                return
+
         screen.fill(color="black")
+
+        for x in drawable:
+            x.draw(screen)
+
         pygame.display.flip()
+        delta_time = clock.tick(60)/1000
+
 
 
 
